8f74749073e8c8d120b3ac535c50787a9e0b884f
Carlos11378 <litteralinea@gmail.com>
2025-12-04 14:34:03 -0300

    Create Home component with NewsFeed integration

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewsFeed from '../components/NewsFeed';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Últimas Notícias</h2>
+      <NewsFeed />
+    </div>
+  );
+};
+
+export default Home;
